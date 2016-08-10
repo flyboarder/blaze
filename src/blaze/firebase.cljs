b4931bdf3e6f99bb9119142fdd8f49a038ebcbc4
Matthew Ratzke <matt@degree9.io>
2016-08-10 13:56:44 -0600

    firebase.cljs

--- src/blaze/firebase.cljs
+++ src/blaze/firebase.cljs
@@ -3,15 +3,3 @@
             [firebase-cljs.core :as fb]
             [hoplon.firebase :as hfb])
   (:require-macros [adzerk.env :as env]))
-
-(env/def
-  BLAZE_API_KEY        :required
-  BLAZE_AUTH_DOMAIN    :required
-  BLAZE_DATABASE_URL   :required
-  BLAZE_STORAGE_BUCKET :required)
-
-(fb/init
-  {:apiKey BLAZE_API_KEY
-   :authDomain BLAZE_AUTH_DOMAIN
-   :databaseURL BLAZE_DATABASE_URL
-   :storageBucket BLAZE_STORAGE_BUCKET})
