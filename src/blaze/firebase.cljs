c82240b1d1b915077b4f50d66988c4937fb2b9bf
Matthew Ratzke <matt@degree9.io>
2016-08-10 14:40:24 -0600

    firebase.cljs

--- src/blaze/firebase.cljs
+++ src/blaze/firebase.cljs
@@ -1,6 +1,13 @@
 (ns blaze.firebase
   (:require [cljsjs.firebase]
             [firebase-cljs.core :as fb]
+            [firebase-cljs.database :as fbdb]
             [hoplon.firebase :as hfb]))
 
-(def posts (hfb/fb-cell))
+(def auth (fb/get-auth))
+
+(def app (fb/get-app))
+
+(def db (fb/get-db))
+
+(def posts (hfb/fb-cell ()))
