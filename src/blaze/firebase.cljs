53ffeb7532d519b07c5fc6f277fcb0a240257489
Matthew Ratzke <matt@degree9.io>
2016-08-10 14:51:28 -0600

    firebase.cljs

--- src/blaze/firebase.cljs
+++ src/blaze/firebase.cljs
@@ -4,22 +4,24 @@
             [firebase-cljs.database :as fbdb]
             [hoplon.firebase :as hfb]))
 
+;; Firebase ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (def auth (fb/get-auth))
 
 (def app (fb/get-app))
 
 (def db (fb/get-db))
 
-(def posts (hfb/fb-cell (fbdb/get-in db [:posts])))
+;; Firebase Public References ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(def posts      (hfb/fb-cell (fbdb/get-in db [:posts])))
 
-(def settings (hfb/fb-cell (fbdb/get-in db [:settings])))
+(def settings   (hfb/fb-cell (fbdb/get-in db [:settings])))
 
 (def categories (hfb/fb-cell (fbdb/get-in db [:categories])))
 
-(def tags (hfb/fb-cell (fbdb/get-in db [:tags])))
+(def tags       (hfb/fb-cell (fbdb/get-in db [:tags])))
 
-(def pages (hfb/fb-cell (fbdb/get-in db [:pages])))
+(def pages      (hfb/fb-cell (fbdb/get-in db [:pages])))
 
 (def navigation (hfb/fb-cell (fbdb/get-in db [:navigation])))
 
-(def apps (hfb/fb-cell (fbdb/get-in db [:apps])))
+(def apps       (hfb/fb-cell (fbdb/get-in db [:apps])))
