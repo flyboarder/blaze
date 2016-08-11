7d3f86a750074c051a154e2174d449eb9d054905
Matthew Ratzke <matt@degree9.io>
2016-08-11 12:00:17 -0600

    routing.cljs

---
+++ src/blaze/routing.cljs
@@ -0,0 +1,9 @@
+(ns blaze.routing)
+
+(def routes
+  ["" {#{"" "/"} :index
+       "tags" {#{"" "/"} :tags}
+       "admin" #{"" "/"} :admin}
+       "pages" {#{"" "/"} :pages}
+       "posts" {#{"" "/"} :posts}
+       "categories" {#{"" "/"} :categories}])
