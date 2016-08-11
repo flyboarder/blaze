95f768bffb90a33e08e0b40c860d8ac835d4caee
Matthew Ratzke <matt@degree9.io>
2016-08-11 01:04:29 -0600

    firebase.cljs

--- src/blaze/firebase.cljs
+++ src/blaze/firebase.cljs
@@ -5,23 +5,23 @@
             [hoplon.firebase :as hfb]))
 
 ;; Firebase ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(def ^:dynamic auth (fb/get-auth))
+(def ^:dynamic *auth* (fb/get-auth))
 
-(def ^:dynamic app  (fb/get-app))
+(def ^:dynamic *app*  (fb/get-app))
 
-(def ^:dynamic db   (fbdb/get-in (fb/get-db) [:_blaze])
+(def ^:dynamic *db*   (fbdb/get-in (fb/get-db) [:_blaze])
 
 ;; Firebase Public References ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(def posts      (hfb/fb-cell (fbdb/get-in db [:posts])))
+(def posts      (hfb/fb-cell (fbdb/get-in *db* [:posts])))
 
-(def settings   (hfb/fb-cell (fbdb/get-in db [:settings])))
+(def settings   (hfb/fb-cell (fbdb/get-in *db* [:settings])))
 
-(def categories (hfb/fb-cell (fbdb/get-in db [:categories])))
+(def categories (hfb/fb-cell (fbdb/get-in *db* [:categories])))
 
-(def tags       (hfb/fb-cell (fbdb/get-in db [:tags])))
+(def tags       (hfb/fb-cell (fbdb/get-in *db* [:tags])))
 
-(def pages      (hfb/fb-cell (fbdb/get-in db [:pages])))
+(def pages      (hfb/fb-cell (fbdb/get-in *db* [:pages])))
 
-(def navigation (hfb/fb-cell (fbdb/get-in db [:navigation])))
+(def navigation (hfb/fb-cell (fbdb/get-in *db* [:navigation])))
 
-(def apps       (hfb/fb-cell (fbdb/get-in db [:apps])))
+(def apps       (hfb/fb-cell (fbdb/get-in *db* [:apps])))
